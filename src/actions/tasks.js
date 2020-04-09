bebbf21ecb956174d5b917491ee0f5f12b7e1e4a
Aishwarya Bannimatti <aishbannimatti@gmail.com>
2020-04-10 01:29:39 +0530

    Fixed #2 - delete task throwing an error in console

--- src/actions/tasks.js
+++ src/actions/tasks.js
@@ -72,7 +72,6 @@ export const reorderTasks = (reorderedTasks) => (dispatch, getState) => {
         })
 }
 
-// @todo Fix the error thrown in console after delete
 export const deleteTask = (taskId) => (dispatch) => {
     db.collection('tasks')
         .doc(taskId)
